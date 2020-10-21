842b429781a1f63574bddfe4dee8df9fe121556d
Bimo dwien Prabowo <bimodwienprabowo@Bimos-MacBook-Air.local>
2020-10-21 21:37:12 +0700

    pair project: test

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes/index')
+
+app.use('view engine', 'ejs')
+app.use(express.urlencoded({extended : true}))
+app.use(routes)
+
+app.listen(port , () =>{
+  console.log(`App berjalan di port ${port}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const routes = require('express').Router()
+const employeeRoutes = require('./employees')
+const projectRoutes = require('./projects')
+
+
+module.exports = routes
