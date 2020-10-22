201c8f2697a5cb2e04905fca1f4a21cb8c980813
rey-h8 <rey-h8@gmail.com>
2020-10-22 17:21:18 +0700

    update Controller

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -58,51 +58,6 @@ class Controller {
 
   }
 
-  static employees(req, res) {
-
-  }
-
-  static projects(req, res) {
-
-
-  }
-
-  static getAddEmployee(req, res) {
-
-  }
-
-  static postAddEmployee(req, res) {
-
-  }
-
-  static getEditEmployee(req, res) {
-
-  }
-
-  static postEditEmployee(req, res) {
-
-  }
-
-  static getProjectAdd(req, res) {
-
-  }
-
-  static postProjectAdd(req, res) {
-
-  }
-
-  static getProjectsOpen(req, res) {
-
-  }
-
-  static getProjectsMine(req, res) {
-
-  }
-
-  static getProjectsUnassigned(req, res) {
-
-  }
-
 
 }
 
